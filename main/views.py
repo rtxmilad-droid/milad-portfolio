c8aa4e5095632c1ac644b080f22f6d0ad1951fa1
miladvyd <rtxmilad@gmail.com>
2026-07-14 23:31:29 +0330

    update design

--- main/views.py
+++ main/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Project, ContactMessage
-
-
-
 
 
 def home(request):
@@ -11,34 +8,18 @@ def home(request):
     projects = Project.objects.all()
 
     return render(request, "home.html", {
-
         "projects": projects
-
     })
-
-
-
-
 
 
 
 def project_detail(request, id):
 
-    project = get_object_or_404(
-        Project,
-        id=id
-    )
-
+    project = get_object_or_404(Project, id=id)
 
     return render(request, "project_detail.html", {
-
         "project": project
-
     })
-
-
-
-
 
 
 
@@ -48,34 +29,23 @@ def resume(request):
 
 
 
-
-
-
-
 def contact(request):
-
 
     if request.method == "POST":
 
-
-        name = request.POST["name"]
-
-        email = request.POST["email"]
-
-        message = request.POST["message"]
-
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
 
 
         ContactMessage.objects.create(
-
             name=name,
-
             email=email,
-
             message=message
-
         )
 
+
+        return redirect("/contact/")
 
 
     return render(request, "contact.html")
