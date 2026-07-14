a0ef0896749764c38c22e107e5f95c205e0eda39
miladvyd <rtxmilad@gmail.com>
2026-07-14 23:56:00 +0330

    test css

--- milad_portfolio/settings.py
+++ milad_portfolio/settings.py
@@ -123,13 +123,14 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 MEDIA_URL = "/media/"
-
 MEDIA_ROOT = BASE_DIR / "media"
