cc19ffd8a9e9cf3684e4cdb8b792fab6f9e02afb
miladvyd <rtxmilad@gmail.com>
2026-07-14 23:35:00 +0330

    test html

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-
     path("", views.home, name="home"),
-
-    path("project/<int:id>/", views.project_detail, name="project_detail"),
-
-    path("resume/", views.resume, name="resume"),
 
     path("contact/", views.contact, name="contact"),
 
+    path("resume/", views.resume, name="resume"),
+    
+    path("project/<int:id>/", views.project_detail, name="project_detail"),
 ]
